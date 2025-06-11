aeda649cf2ebacf814d679a5307b55fab3ed2a1b
Pamela Badermann <badermann.pamela@gmail.com>
2025-06-10 23:34:49 -0300

    criação do endpoint GetProduct por Type ou Brand

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97e3d737954a6fcd5e8e7fe8a463c43a38b5c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018f907bd3cf7a105a2240899efb1a681bf38a64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
