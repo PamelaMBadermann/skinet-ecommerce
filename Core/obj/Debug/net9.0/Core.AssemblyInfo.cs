a97e3d737954a6fcd5e8e7fe8a463c43a38b5c41
Pamela Badermann <badermann.pamela@gmail.com>
2025-06-10 21:32:35 -0300

    criando seed de dados

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3724edb104e29706d9081e8629d93a1079750257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4eec5d79f6a6a8fede5a0a02003c8bb1b8bebaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
