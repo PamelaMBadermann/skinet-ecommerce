018f907bd3cf7a105a2240899efb1a681bf38a64
Pamela Badermann <badermann.pamela@gmail.com>
2025-06-10 23:17:21 -0300

    criação dos endpoints GetTypes e GetBrands

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4eec5d79f6a6a8fede5a0a02003c8bb1b8bebaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97e3d737954a6fcd5e8e7fe8a463c43a38b5c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4eec5d79f6a6a8fede5a0a02003c8bb1b8bebaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97e3d737954a6fcd5e8e7fe8a463c43a38b5c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
