0331f67f43829400dd21815019b5725314d137ea
Pamela Badermann <badermann.pamela@gmail.com>
2025-06-20 00:14:07 -0300

    adicionando projeção na specification

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a1a7111f6008524d16913b98855a32edf938ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43356c35ff7f8436d924abddbfecb8249c44c688")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
