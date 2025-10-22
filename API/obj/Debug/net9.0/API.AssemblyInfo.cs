b313aa097b9504f68fc58a52825eb2aebe02fa59
Pamela Badermann <badermann.pamela@gmail.com>
2025-10-22 17:18:38 -0300

    finalizando projeções na specification

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50616f31963130c9b4d7739c38ecde2873023a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25229c7b397d3fb2450372a34f2e9e1cbd5e04a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
